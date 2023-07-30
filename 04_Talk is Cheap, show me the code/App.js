a8b11dabcc36131c7b8706c6bcf590a6c9c9b3f5
NiteshJ1998 <niteshjaiswar10@gmail.com>
2023-07-30 06:45:45 +0530

    added App.js

--- 04_Talk is Cheap, show me the code/App.js
+++ 04_Talk is Cheap, show me the code/App.js
@@ -9,6 +9,8 @@ import ReactDOM from "react-dom/client";
  * - Search
  * - RestaurantContainer
  *   - RestaurantCard
+ *      - Image
+ *      - Name of Restaurant, Star Rating, cuisine, delivery time
  * Footer
  * - Copyright
  * - Links
@@ -39,10 +41,35 @@ const Header = () => {
   );
 };
 
+const RestaurantCard = () => {
+  return (
+    <div
+      className="res-card"
+      style={{
+        backgroundColor: "#f0f0f0",
+      }}
+    >
+      <h3>Dizvi Chinese Restaurants</h3>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
+      <Body />
     </div>
   );
 };
